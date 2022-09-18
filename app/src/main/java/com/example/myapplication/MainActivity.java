29a0c31ad94c1c39c6f23bd315e908edaa245111
yungThe <74132467+yungThe@users.noreply.github.com>
2022-09-18 23:17:36 +0700

    change brush size, change color

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -2,11 +2,17 @@ package com.example.myapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.util.DisplayMetrics;
+import android.graphics.Color;
+import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.SeekBar;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
@@ -19,10 +25,31 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        //setContentView(R.layout.change_draw_specs);
         setContentView(R.layout.activity_main);
+        //LayoutInflater layoutInflater = getLayoutInflater();
+        //layoutInflater.inflate(R.layout.change_draw_specs);
+        SeekBar seekbar = findViewById(R.id.seek_bar);
+        seekbar.setMax(200);
+        seekbar.setProgress(10);
+        seekbar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int i, boolean b) {
+                paintView.BRUSH_SIZE = i;
+            }
 
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+
+            }
+        });
         paintView = findViewById(R.id.paint_view);
-
+        paintView.COLOR_PEN = Color.BLACK;
         DisplayMetrics metrics = new DisplayMetrics();
         getWindowManager().getDefaultDisplay().getMetrics(metrics);
         paintView.init(metrics);
@@ -34,6 +61,8 @@ public class MainActivity extends AppCompatActivity {
         menuInflater.inflate(R.menu.options_menu, menu);
         return super.onCreateOptionsMenu(menu);
     }
+
+
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
@@ -53,6 +82,12 @@ public class MainActivity extends AppCompatActivity {
                 Toast.makeText(this, "Canvas Empty!", Toast.LENGTH_SHORT).show();
                 return true;
 
+            case R.id.photo :
+                Intent intent = new Intent(Intent.ACTION_PICK,
+                        android.provider.MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+                startActivityForResult(intent, 1);
+                return true;
+
             case R.id.save:
                 paintView.save();
                 Toast.makeText(this, "Saved!", Toast.LENGTH_SHORT).show();
@@ -61,11 +96,62 @@ public class MainActivity extends AppCompatActivity {
             case R.id.back:
                 paintView.imageReverse();
                 //Toast.makeText(this, "Reversed!", Toast.LENGTH_SHORT).show();
+                return true;
+            case R.id.color_black:
+                Toast.makeText(this,"Black",Toast.LENGTH_SHORT).show();
+                paintView.COLOR_PEN = Color.BLACK;
+                paintView.pen();
+                return true;
+            case R.id.color_red:
+                Toast.makeText(this,"Red",Toast.LENGTH_SHORT).show();
+                paintView.COLOR_PEN = Color.RED;
+                paintView.pen();
+                return true;
+            case R.id.color_yellow:
+                Toast.makeText(this,"Yellow",Toast.LENGTH_SHORT).show();
+                paintView.COLOR_PEN = Color.YELLOW;
+                paintView.pen();
+                return true;
 
+            case R.id.color_blue:
+                Toast.makeText(this,"Blue",Toast.LENGTH_SHORT).show();
+                paintView.COLOR_PEN = Color.BLUE;
+                paintView.pen();
+                return true;
+            case R.id.color_green:
+                Toast.makeText(this,"Green",Toast.LENGTH_SHORT).show();
+                paintView.COLOR_PEN = Color.GREEN;
+                paintView.pen();
                 return true;
         }
 
-
         return super.onOptionsItemSelected(item);
+    }
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        // Kiểm tra requestCode có trùng với REQUEST_CODE vừa dùng
+        if(requestCode == 1 && resultCode == RESULT_OK && data != null){
+
+
+            //lấy Uri của hình ảnh đã chọn
+            Uri url_value = data.getData();
+
+            //thông báo
+            Toast.makeText(this, url_value.toString(), Toast.LENGTH_LONG).show();
+            //mở bitmap bằng hình ảnh
+
+
+            try {
+                paintView.importPhoto(url_value);
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+
+
+        }else {
+            // DetailActivity không thành công, không có data trả về.
+        }
     }
 }
--- app/src/main/java/com/example/myapplication/PaintView.java
+++ app/src/main/java/com/example/myapplication/PaintView.java
@@ -1,7 +1,9 @@
 package com.example.myapplication;
 
+import android.content.ContentResolver;
 import android.content.Context;
 import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
@@ -9,6 +11,7 @@ import android.graphics.Path;
 import android.media.MediaScannerConnection;
 import android.net.Uri;
 import android.os.Environment;
+import android.provider.MediaStore;
 import android.util.AttributeSet;
 import android.util.DisplayMetrics;
 import android.util.Log;
@@ -18,14 +21,16 @@ import android.view.View;
 import androidx.annotation.Nullable;
 
 import java.io.File;
+import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Random;
 
 public class PaintView extends View {
 
     public int BRUSH_SIZE = 10;
-    public static final int COLOR_PEN = Color.BLACK;
+    public static int COLOR_PEN = Color.BLACK;
     public static final int COLOR_ERASER = Color.WHITE;
     public static final int DEFAULT_BG_COLOR = Color.WHITE;
     private static final float TOUCH_TOLERANCE = 4;
@@ -76,7 +81,7 @@ public class PaintView extends View {
 
     public void eraser(){
 
-        BRUSH_SIZE = 300;
+        BRUSH_SIZE = 200;
         currentColor = COLOR_ERASER;
     }
 
@@ -87,17 +92,51 @@ public class PaintView extends View {
         invalidate();
     }
 
-    public void importPhoto(){
 
+    public void importPhoto(Uri url_value ){
+        Bitmap tempBitmap;
+
+        try {
+            //tempBitmap is Immutable bitmap,
+            //cannot be passed to Canvas constructor
+            tempBitmap = BitmapFactory.decodeStream(
+                    getContentResolver().openInputStream(url_value));
+            Bitmap.Config config;
+            if(tempBitmap.getConfig() != null){
+                config = tempBitmap.getConfig();
+                //mCanvas.drawBitmap(tempBitmap, 0, 0, null);
+            }else{
+                config = Bitmap.Config.ARGB_8888;
+            }
+            //bitmapMaster is Mutable bitmap
+            mBitmap = Bitmap.createBitmap(
+                    tempBitmap.getWidth(),
+                    tempBitmap.getHeight(),
+                    config);
+            mCanvas = new Canvas(mBitmap);
+            mCanvas.drawBitmap(tempBitmap, 0, 0, null);
+        } catch (FileNotFoundException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+
+
+    }
+
+    private ContentResolver getContentResolver() {
+        return null;
+    }
+
+    private Context getApplicationContext() {
+        return this.getContext();
     }
 
     private void SaveImage(Bitmap finalBitmap) {
         String root = Environment.getExternalStoragePublicDirectory(
                 Environment.DIRECTORY_PICTURES).toString();
-        File myDir = new File(root + "/saved_images");
+        File myDir = new File(root + "/Draw_Project");
         myDir.mkdirs();
         Random generator = new Random();
-
         int n = 10000;
         n = generator.nextInt(n);
         String fname = "Image-"+ n +".jpg";
@@ -110,7 +149,6 @@ public class PaintView extends View {
             //     Uri.parse("file://"+ Environment.getExternalStorageDirectory())));
             out.flush();
             out.close();
-
         } catch (Exception e) {
             e.printStackTrace();
         }
@@ -118,7 +156,6 @@ public class PaintView extends View {
         // immediately available to the user.
         MediaScannerConnection.scanFile(getContext(), new String[]{file.toString()}, null,
                 new MediaScannerConnection.OnScanCompletedListener() {
-
                     @Override
                     public void onScanCompleted(String path, Uri uri) {
                         Log.i("ExternalStorage", "Scanned " + path + ":");
@@ -153,7 +190,6 @@ public class PaintView extends View {
             mPaint.setColor(fp.getColor());
             mPaint.setStrokeWidth(fp.getStrokeWidth());
             mPaint.setMaskFilter(null);
-
             mCanvas.drawPath(fp.getPath(), mPaint);
         }
 
